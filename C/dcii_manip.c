d5cae15095c442814d47ca2b85e68b78d4fc73bd
RealDoigt <57451013+RealDoigt@users.noreply.github.com>
2023-03-27 10:42:07 -0400

    cscompare in c

--- C/dcii_manip.c
+++ C/dcii_manip.c
@@ -1,1 +1,19 @@
 #include "dcii_manip.h"
+
+bool compare_csstr(uchar* left, uchar* right, size_t leftLength, size_t rightLength)
+{
+    if (leftLength != rightLength) return false;
+    
+    for (size_t i; i < leftLength; ++i)
+        if (left[i] != right[i]) return false;
+    
+    return true;
+}
+
+bool compare_ntcsstr(uchar* left, uchar* right)
+{
+    for (size_t i; left[i] != 0 && right[i] != 0; ++i)
+        if (left[i] != right[i]) return false;
+        
+    return true;
+}
