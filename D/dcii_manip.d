01674cb4bb0628f45d393454ca98bf47dba79e1c
RealDoigt <57451013+RealDoigt@users.noreply.github.com>
2023-03-27 10:20:32 -0400

    case sensitive comparison

--- D/dcii_manip.d
+++ D/dcii_manip.d
@@ -1,1 +1,11 @@
- 
+module dcii_manip;
+
+auto compareCaseSensitive(ref ubyte[] left, ref ubyte[] right)
+{
+    if (left.length != right.length) return false;
+    
+    for (size_t i; i < left.length; ++i)
+        if (left[i] != right [i]) return false;
+        
+    return true;
+}
